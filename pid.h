70ec61f97f2fa61898ca2371d679fc88a559b64c
Bruno Pinho <bpinhosilva@gmail.com>
2015-05-12 01:07:13 -0300

    Atualização licença open source
    Adição da descrição da licença de software livre nos arquivos do
    projeto.

--- pid.h
+++ pid.h
@@ -1,7 +1,21 @@
 /*
-  pid.h - Library for PID control.
-  Created by Bruno Pinho, February 21, 2015.
-  Released into the public domain.
+  pid.h - Library for PID control
+  Copyright (c) 2015 Bruno Sampaio Pinho da Silva.  All right reserved.
+  E-mail address: bpinhosilva@gmail.com
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #ifndef PID_H
